72d1c7fe6f0bffbf0683cdb1a5ee495e56560270
Gabries <105074547+gabrieszin@users.noreply.github.com>
2023-02-06 12:35:58 -0300

    Criando métodos utilitários e verificando se os campos de hóspede foram preenchidos

---
+++ src/views/utilitarios/Utilitarios.java
@@ -0,0 +1,16 @@
+package views.utilitarios;
+
+public class Utilitarios {
+    public static boolean isEmpty(String string) {
+        string = string.trim();
+        return (string == null || string.length() == 0);
+    }
+
+    public static boolean isClear(String string) {
+        return (string.length() == 0);
+    }
+
+    public static boolean isNull(String string) {
+        return (string == null);
+    }
+}
