7f69f1aad290b565aa18cb4be8fd0e53a0ed37fe
Gabries <105074547+gabrieszin@users.noreply.github.com>
2023-02-07 12:22:36 -0300

    Adicionando Main

---
+++ src/br/com/hotelAlura/Main.java
@@ -0,0 +1,13 @@
+package br.com.hotelAlura;
+
+import br.com.hotelAlura.views.MenuPrincipal;
+
+import javax.swing.*;
+
+public class Main {
+
+    //Aciona o Menu Principal
+    public static void main(String[] args) {
+        MenuPrincipal.main(null);
+    }
+}
